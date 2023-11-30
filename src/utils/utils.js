20119af6251995ce27dfe913b12668c36db13484
wechuli <wechulipaul@yahoo.com>
2023-11-30 12:46:02 +0300

    change source

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,7 +5,8 @@ function parseTaskInput() {
   const input = core.getInput("tasks") || "context";
   //
   let tasksArray = input.split(",").map((task) => task.trim());
-  // validate that we support all the tasks
+
+  
   for (const task of tasksArray) {
     if (!allTasks[task]) {
       throw new Error(
